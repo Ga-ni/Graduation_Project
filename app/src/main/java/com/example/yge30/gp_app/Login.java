4e330a88e80dddef6a60efaacdbad538bd3dc486
Ga-ni <ggaeun3075@gmail.com>
2018-03-21 15:57:56 +0900

    make login view

--- app/src/main/java/com/example/yge30/gp_app/Login.java
+++ app/src/main/java/com/example/yge30/gp_app/Login.java
@@ -2,12 +2,19 @@ package com.example.yge30.gp_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class Login extends AppCompatActivity {
+
+    EditText ET_pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+
     }
 }
