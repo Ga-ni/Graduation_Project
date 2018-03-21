e66bd4758216d586dfd2f981fc06930277b216e9
Ga-ni <ggaeun3075@gmail.com>
2018-03-21 11:52:04 +0900

    initial commit

---
+++ app/src/main/java/com/example/yge30/gp_app/Login.java
@@ -0,0 +1,13 @@
+package com.example.yge30.gp_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+    }
+}
